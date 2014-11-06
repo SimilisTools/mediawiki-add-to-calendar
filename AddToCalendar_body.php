60a51ddbf47dc0402d5b2bcad7fa086345f56010
toniher <toniher@cau.cat>
2014-11-06 18:41:41 +0100

    params

--- AddToCalendar_body.php
+++ AddToCalendar_body.php
@@ -2,6 +2,8 @@
 
 class AddToCalendar {
 
+	private static $attrs_ref = array( "start", "end", "address", "description", "title", "duration" );
+
 	/*
 	 * @param $parser Parser
 	 * @param $frame PPFrame
@@ -10,7 +12,9 @@ class AddToCalendar {
 	 */
 	 
 	public static function process_AddToCalendar( &$parser, $frame, $args ) {
-
+		
+		
+		
 		$output = "";
 		return $parser->insertStripItem( $output, $parser->mStripState );
 	}
