f972b445690de1d184c9ed5fc9c46e680a3ca291
Toni Hermoso Pulido <toniher@cau.cat>
2014-11-07 11:07:01 +0100

    adding to calendar

--- libs/addtocalendar.js
+++ libs/addtocalendar.js
@@ -1,3 +1,31 @@
-/*globals */
-/* content below */
+/*globals $ document createCalendar */
 
+$(document).ready( function() {
+	var mandatory = ["title", "start", "end"]; //Mandatory elems
+	
+	$(".addtocalendar").each( function( i ) {
+		
+		var eventData = $(this).data();
+		
+		var check = 0;
+		
+		for ( var c=0; c < mandatory.length; c=c+1 ) {
+			if ( mandatory[c] in eventData ) {
+				check = check + 1;
+			}
+		}
+		
+		if ( check >= mandatory.length ) {
+		
+			var linkCalendar = createCalendar({
+				options: {
+					class: 'addtocalendar_link',
+				},
+				data: eventData
+			});
+			 
+			$(this).append( linkCalendar );
+		}
+	});
+	
+});
